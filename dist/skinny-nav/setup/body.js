ffa781e2cdabe9e1500a9561d6c9ff3a34d5b0fe
Chris Ferdinandi <cferdinandi@tibco.com>
2018-11-20 10:43:38 -0500

    version bump

--- dist/skinny-nav/setup/body.js
+++ dist/skinny-nav/setup/body.js
@@ -1,7 +1,7 @@
 /*!
  * Templates & Initializations
  * portal-theme v1.0.0
- * Portal Theme Starter Kit v2.7.15 - Default Theme
+ * Portal Theme Starter Kit v2.7.16 - Default Theme
  * http://github.com/mashery/portal-theme-starter-kit
  */
 
